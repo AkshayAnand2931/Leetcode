a909667ce6e5f8776db4165f7c513675b5515e71
Akshay Anand <akshayanand.abbur@gmail.com>
2025-09-15 21:05:59 +0530

    Max consequtive ones

---
+++ Arrays/485)MaxConsecutiveOnes.cpp
@@ -0,0 +1,31 @@
+/*
+    The problem is to find the max count of consecutive ones
+    in an array of zeros and ones.
+*/
+
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+        int max_con = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 1)
+            {
+                count++;
+            }
+            else
+            {
+                count = 0;
+            }
+            max_con = max(count, max_con);
+        }
+        return max_con;
+    }
+};
