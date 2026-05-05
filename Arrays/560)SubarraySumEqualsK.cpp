ea5630639000c5f57d072bee8e87006ffcf537df
Akshay Anand <akshayanand.abbur@gmail.com>
2026-05-05 16:05:23 +0530

    Array Problems

--- Arrays/560)SubarraySumEqualsK.cpp
+++ Arrays/560)SubarraySumEqualsK.cpp
@@ -33,3 +33,26 @@ public:
         return count;
     }
 };
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum = 0;
+        unordered_map<int, int> prefix_sum;
+        int count = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            sum += nums.at(i);
+
+            if(sum == k)
+                count++;
+
+            if(prefix_sum.find(sum - k) != prefix_sum.end())
+                count += prefix_sum[sum - k];
+            
+            prefix_sum[sum]++;
+        }
+
+        return count;
+    }
+};
