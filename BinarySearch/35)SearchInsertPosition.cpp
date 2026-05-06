755a213b54f12915146086427488a25e5c95a440
Akshay Anand <akshayanand.abbur@gmail.com>
2026-05-06 16:04:03 +0530

    Binary Search Problems

---
+++ BinarySearch/35)SearchInsertPosition.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        const int size = nums.size();
+        int low = 0;
+        int high = size - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) return mid;
+            else if(target > nums[mid]) low = mid + 1;
+            else high = mid - 1;
+        }
+
+        return low;
+    }
+};
---
+++ BinarySearch/704)BinarySearch.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        const int size = nums.size();
+        int low = 0;
+        int high = size - 1;
+
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(nums[mid] == target) return mid;
+            else if (target < nums[mid]) high = mid - 1;
+            else low = mid + 1;
+        }
+
+        return -1;
+    }
+};
